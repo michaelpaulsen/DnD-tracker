33191ebf4a0d5f8ae967b243f5ae198380a7ada1
michaelpaulsen <michael.j.paulsen@gmail.com>
2021-06-11 14:30:50 -0700

    made a class to get the get pramas on the server side
    since the get pramas don't exist on the server side I hade to modify the factory to take a string and use that rather then use the location.query property other than that it is a functional copy of the client side Query class

---
+++ DnD tracker/Query-svs.js
@@ -0,0 +1,24 @@
+﻿// QUERY SELECTOR GETER
+// © Michael Paulsen 2019 - 2021
+exports.Query = function(str) {
+    console.log();
+    let obj = {}; 
+    obj.querys = str.substring(1,str.length).split('&');
+    for (let x = 0; x < obj.querys.length; x++) {
+        let param = obj.querys[x].split('='); 
+        let name = param[0];
+        let value = param[1];
+        if (value == "true") {
+            obj[name] = true;
+            continue;
+        } else if (value == 'false') {
+            obj[name] = false;
+            continue;
+        } else if (!isNaN(parseInt(value))) {
+            obj[name] = parseInt(value);
+            continue;
+        }
+        obj[name] = value;
+    }
+    return obj;
+}
